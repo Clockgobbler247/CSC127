98d82546d1e02c8920349c6b05f108f3fb2d72ac
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2023-10-18 19:00:35 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,6 +4,26 @@
 # Backup to /dev/sdc 75GB Backup Drive mounted to /backups
 #
 ####################################
+
+# Aquire the UUID for /dev/sdc
+result=$(sudo blkid -o value -s UUID "/dev/sdc")
+
+# Print the UUID to check if it's obtained correctly
+echo "UUID: $result"
+
+# Define the line to check for in fstab
+desired_line1="UUID=$result  /home ext4  defaults  0 2"
+
+# Check if the line already exists in the fstab file
+if grep -Fxq "$desired_line1" /etc/fstab; then
+  echo "The line is already present in /etc/fstab."
+else
+  # Add the line to fstab using echo and append (>>) operator
+  echo "$desired_line1" | sudo tee -a /etc/fstab > /dev/null
+  echo "The line has been added to /etc/fstab."
+fi
+
+
 # What to backup. 
 
 backup_files="/"
