9f74031418090b0a464413bb7650b1bb9ca36156
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2023-10-19 21:28:01 -0500

    Create diff_usr_backup.sh
    Added new script

---
+++ diff_usr_backup.sh
@@ -0,0 +1,48 @@
+# What to backup. 
+
+backup_files="/home/*"
+
+# Where to backup to
+
+dest="/backups/usr_backup/diff_backup"
+
+
+# Create archive filename
+
+day=$(date +%m_%d_%Y)
+
+hostname=$(hostname -s)
+
+archive_file="$hostname-usr_data-$day-diff"
+
+full_backup="/backups/usr_backup/full_backup/new/*
+
+# Print variable values for debugging
+
+echo "dest: $dest"
+
+echo "archive_file: $archive_file"
+
+#Create Directory for backup
+sudo mkdir $dest/$archive_file
+
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+
+date
+
+echo
+
+# Backup the files using tar.
+#rsync $dest/$archive_file $backup_files
+sudo rsync -aAXHv --link-dest=$full_backup --delete --exclude=.cache/* --exclude=tmp/* $backup_files $dest/$archive_file
+
+echo
+
+echo "Backup finished"
+
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
