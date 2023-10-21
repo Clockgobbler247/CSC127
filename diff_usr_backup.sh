a71879738328b49904c8ca45a19f542370bee420
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2023-10-20 22:20:24 -0500

    Update diff_usr_backup.sh

--- diff_usr_backup.sh
+++ diff_usr_backup.sh
@@ -1,27 +1,23 @@
+#!/bin/bash
 # What to backup. 
-
 backup_files="/home/*"
 
-# Where to backup to
 
+# Where to backup to
 dest="/backups/usr_backup/diff_backup"
 
 
 # Create archive filename
-
 day=$(date +%m_%d_%Y)
-
 hostname=$(hostname -s)
-
 archive_file="$hostname-usr_data-$day-diff"
+full_backup="/backups/usr_backup/full_backup/new/Team-A-Usr-data_full_backup"
 
-full_backup="/backups/usr_backup/full_backup/new/Team-A-Usr-data_full_backup
 
 # Print variable values for debugging
-
 echo "dest: $dest"
+echo "archive_file: $archive_filea
 
-echo "archive_file: $archive_file"
 
 #Create Directory for backup
 sudo mkdir $dest/$archive_file
@@ -29,20 +25,16 @@ sudo mkdir $dest/$archive_file
 
 # Print start status message.
 echo "Backing up $backup_files to $dest/$archive_file"
-
 date
-
 echo
 
-# Backup the files using tar.
+
 #rsync $dest/$archive_file $backup_files
 sudo rsync -aAXHv --link-dest=$full_backup --delete --exclude=.cache/* --exclude=tmp/* $backup_files $dest/$archive_file
-
 echo
-
 echo "Backup finished"
-
 date
+
 
 # Long listing of files in $dest to check file sizes.
 ls -lh $dest
