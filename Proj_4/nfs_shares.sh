addac568bdc139d578b5dc8654037a89a812b0c1
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2023-11-09 19:13:16 -0600

    Update nfs_shares.sh

--- Proj_4/nfs_shares.sh
+++ Proj_4/nfs_shares.sh
@@ -38,7 +38,7 @@ result=$(sudo blkid -o value -s UUID "/dev/sdd")
 echo "UUID: $result"
 
 # Define the line to check for in fstab
-desired_line1="UUID=$result  /backups ext4  defaults  0 2"
+desired_line1="UUID=$result  /shares ext4  defaults  0 2"
 
 # Check if the line already exists in the fstab file
 if grep -Fxq "$desired_line1" /etc/fstab; then
