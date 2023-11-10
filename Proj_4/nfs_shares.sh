5129bc7a1f7375171a09a13999bd019130afd47e
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2023-11-09 18:15:48 -0600

    Update nfs_shares.sh

--- Proj_4/nfs_shares.sh
+++ Proj_4/nfs_shares.sh
@@ -3,7 +3,26 @@
 ########################### Project #4 - NFS shares ################################
 ####################################################################################
 
-##
+# Format newly added disk
+sudo mkfs.ext4 /dev/sdd
+
+# Aquire the UUID for /dev/sdd
+result=$(sudo blkid -o value -s UUID "/dev/sdd")
+
+# Print the UUID to check if it's obtained correctly
+echo "UUID: $result"
+
+# Define the line to check for in fstab
+desired_line1="UUID=$result  /backups ext4  defaults  0 2"
+
+# Check if the line already exists in the fstab file
+if grep -Fxq "$desired_line1" /etc/fstab; then
+  echo "The line is already present in /etc/fstab."
+else
+  # Add the line to fstab using echo and append (>>) operator
+  echo "$desired_line1" | sudo tee -a /etc/fstab > /dev/null
+  echo "The line has been added to /etc/fstab."
+fi
 
 
 ## Update the VM and install Samba
