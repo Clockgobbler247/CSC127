01aeb721702420f33a35726d5ea427ccb861534f
Clockgobbler247 <145526120+Clockgobbler247@users.noreply.github.com>
2023-11-29 21:44:15 -0600

    Update monitor_auth.sh

--- Proj_5/monitor_auth.sh
+++ Proj_5/monitor_auth.sh
@@ -5,4 +5,4 @@
 #####################
 
 ## Parse out the information and send to a file
-sudo tail -n 5000 /var/log/auth.log | grep -E -a "$(date +'%b %e')" | grep -E -a "failed authentication|authentication failure" >> /monitor/exceptions.log
+sudo tail -n 500 /var/log/auth.log | grep -E -a "$(date +'%b %e')" | grep -E -a "failed authentication|authentication failure" >> /monitor/exceptions.log
